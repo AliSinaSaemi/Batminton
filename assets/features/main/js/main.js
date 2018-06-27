6694b8a3a908965f9c6b4aa453582163bd364cf0
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-06-27 21:56:00 +0430

    Everything is ready except responsive

--- assets/features/main/js/main.js
+++ assets/features/main/js/main.js
@@ -3,6 +3,8 @@ $('#owl-one').owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
+  autoplay:true,
+  autoplayTimeout:10000,
   smartSpeed: 900,
   navText: ["<i class='fa fa-chevron-right'></i>", "<i class='fa fa-chevron-left'></i>"],
   responsive: {
@@ -16,4 +18,25 @@ $('#owl-one').owlCarousel({
       items: 1
     }
   }
-})
+});
+$('#owl-two').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  autoplay:true,
+  autoplayTimeout:15000,
+  nav: true,
+  smartSpeed: 1000,
+  navText: ["<i class='fa fa-arrow-right'></i>", "<i class='fa fa-arrow-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
