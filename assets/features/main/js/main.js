0ec4b91357dbb0c06156e3c289fcf9321f588b68
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-06-28 02:16:58 +0430

    Everything is ready even responsive except side navbar.

--- assets/features/main/js/main.js
+++ assets/features/main/js/main.js
@@ -40,3 +40,28 @@ $('#owl-two').owlCarousel({
     }
   }
 });
+$('#owl-three').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  autoplay:true,
+  autoplayTimeout:15000,
+  nav: true,
+  smartSpeed: 1000,
+  navText: ["<i class='fa fa-arrow-right'></i>", "<i class='fa fa-arrow-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
+  $(document).ready(function(){
+    $(".navbar").sticky({topSpacing:0});
+  });
