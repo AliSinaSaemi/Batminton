62e107db7eb89ec22a69a020c071208c56b5fbfd
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-07-19 02:34:15 +0430

    News is ready !

--- assets/features/main/js/main.js
+++ assets/features/main/js/main.js
@@ -89,6 +89,29 @@ $('#owl-four').owlCarousel({
   }
 });
 
+$('#owl-five').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  autoplay:true,
+  autoplayTimeout:10000,
+  nav: true,
+  smartSpeed: 1000,
+  navText: ["<i class='fa fa-angle-right'></i>", "<i class='fa fa-angle-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
+
 /*=======================
 
         Sticky.js
