c9e0a43f675d6d3abf661de7a4e9ff1b123d125d
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-07-27 00:24:13 +0430

    Bugs are clear!

--- assets/features/main/js/main.js
+++ assets/features/main/js/main.js
@@ -133,6 +133,28 @@ $('#owl-six').owlCarousel({
   }
 });
 
+$('#owl-seven').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  autoplay:true,
+  autoplayTimeout:10000,
+  nav: true,
+  smartSpeed: 1000,
+  navText: ["<i class='fa fa-angle-right'></i>", "<i class='fa fa-angle-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
 
 /*=======================
 
