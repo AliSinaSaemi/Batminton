78df9b4b07c0de3db27b6f3befe3697f4fd563b8
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-07-29 02:51:47 +0430

    Shop and product can be buy or watch

--- assets/features/main/js/main.js
+++ assets/features/main/js/main.js
@@ -154,6 +154,27 @@ $('#owl-seven').owlCarousel({
     }
   }
 });
+$('#owl-eight').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 10000,
+  nav: false,
+  smartSpeed: 1000,
+  navText: ["<i class='fa fa-angle-right'></i>", "<i class='fa fa-angle-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
 
 
 /*=======================
